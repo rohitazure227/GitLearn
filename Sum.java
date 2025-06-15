3ab909b39cbc004873140d3d114aa731cc52bf89
rohit227 <rohitnagpal78@hotmail.com>
2025-06-15 00:37:20 -0400

    added Sum.java

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int num1 = 10; // first number
+        int num2 = 20; // second number
+        int sum = num1 + num2;
+
+        System.out.println("The sum is now: " + sum);
+    }
+}
